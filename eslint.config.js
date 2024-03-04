9bcd8df3f21e250485e9e1cf2231dcb2ee954456
tqlong1609 <tqlong1609@gmail.com>
2024-03-04 08:12:03 +0700

    Add prettier-plugin-sort-imports + bundle-analyzer

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,4 @@
+import sortImportPrettier from '@trivago/prettier-plugin-sort-imports'
 import eslintConfigPrettier from 'eslint-config-prettier'
 
-export default [eslintConfigPrettier]
+export default [eslintConfigPrettier, sortImportPrettier]
