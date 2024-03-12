28f75fbab83eea9545a8d2a8301d399e14c39eda
Tran Long <47853881+tqlong1609@users.noreply.github.com>
2024-03-12 16:00:36 +0700

    Add prettier-plugin-sort-imports + bundle-analyzer (#1)

---
+++ src/shared/Component1.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Component1: React.FC = () => {
+  return (
+    <div>
+      <h1>Component1</h1>
+    </div>
+  )
+}
+
+export default Component1
---
+++ src/shared/Component2.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Component2: React.FC = () => {
+  return (
+    <div>
+      <h1>Component2</h1>
+    </div>
+  )
+}
